f7e39c51d639d0d4e45f94aa1d05717542b4fc28
Tony Homfray <a.homfray@exeter.ac.uk>
2021-07-01 16:17:03 +0100

    Add db_credentials.php to define db connection detail constants

---
+++ private/db_credentials.php
@@ -0,0 +1,7 @@
+<?php 
+
+define("DB_HOSTNAME", "localhost");
+define("DB_USERNAME", "et_db_admin");
+define("DB_PASSWORD", "longbutsimplepassword");
+define("DB_DATABASE", "exercise_tracker");
+define("DB_CHARSET", "utf8mb4");
