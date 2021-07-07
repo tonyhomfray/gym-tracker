04673ecdf0f555348bfcfa9ac84e3fd5b18207ef
Tony Homfray <a.homfray@exeter.ac.uk>
2021-07-07 16:21:52 +0100

    Add some placeholder text to make the footer look more like it finally will

--- private/includes/footer.php
+++ private/includes/footer.php
@@ -1,5 +1,9 @@
-<p>!!! FOOTER GOES HERE !!!</p>
+
+<div id="footer"><span>Footer Text Here</span><span>&copy; Tony Homfray 2021</span></div>
+
+</body>
+</html>
 
 <?php 
-db_disconnect($db);
+// db_disconnect($db);
 ?>
