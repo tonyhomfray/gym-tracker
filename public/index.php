bf217cc042a6153d38670dd2e679187397810537
Tony Homfray <a.homfray@exeter.ac.uk>
2021-07-07 10:47:14 +0100

    Add basic styles to identify header and footer. Move some of the skeleton code around for development purposes

--- public/index.php
+++ public/index.php
@@ -1,56 +1,95 @@
 <?php 
 
 require_once('../private/initialise.php');
-echo "Welcome to the Exercise Tracker App!<br/><br/>";
-echo "Public facing homepage<br/><br/>";
-
 
 $username = 'tony';
-$userRecord = User::getUser($username);
+// $userRecord = User::getUser($username);
+// $user = new User($userRecord);
+$user = User::getUser($username)['0'];
 
-echo "<br/>";
-$user = new User($userRecord);
-echo "<br/>";
 
-echo "User Object<br/>";
-    echo "<pre>";
-    var_dump($user);
-    echo "</pre>";
+$lastSessionDate = $user->last_session_date;
+$lastWorkout = $user->last_workout;
+$currentProgram = $user->current_program;
 
-// echo $user->username . "<br/>";
-// echo $user->currentProgram . "<br/>";
-$lastSessionDate = $user->lastSessionDate;
-$lastWorkoutNumber = $user->lastWorkourNumber;
+// $sessionRecord = Session::getSession($username, $lastWorkout);
+// $session = new Session($sessionRecord);
+$session = Session::getSession($username, $lastWorkout)['0'];
 
-// echo "Last workout date: " . $lastSessionDate . "<br/>";
-// echo "Last workout number: " . $lastWorkoutNumber . "<br/>";
 
-$sessionRecord = Session::getSession($username, $lastWorkoutNumber);
-// echo "<pre>";
-// var_dump($sessionRecord);
-// echo "</pre>";
-
-$session = new Session($sessionRecord);
-echo "<pre>";
-var_dump($session);
-echo "</pre>";
-
-// echo ($session->db_id);
-
-$exerciseRecords = Exercise::getExercise($session->db_id);
-// echo "<pre>";
-// var_dump($exerciseRecords);
-// echo "</pre>";
+$exerciseRecords = Exercise::getExercise($session->id);
 
 
 foreach ($exerciseRecords as $exercise ) {
     $exerciseObj = new Exercise($exercise);
     $session->exercises_performed[] = $exerciseObj;
 }
-echo "Here they are!<br/>";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheets/main.css">
+    <title>Exercise Tracker - Home</title>
+</head>
+<body>
+    <nav id="top-nav">My Navigation</nav>
+    
+    <div id="main">
+        <h1>Welcome to the Exercise Tracker App!</h1>
+        <h2>Public facing homepage</h2>
+        <h3>Welcome <?php echo $username; ?></h3>
+
+
+        <div id="summary">
+            <p>Current Program: <?php echo $currentProgram; ?></p>
+            <p>Last Workout: <?php echo $lastSessionDate; ?></p>
+        </div>
+
+        <div>
+            <?php 
+            foreach ($session->exercises_performed as $exercise ) {
+                var_dump($exercise);
+                echo "<br/>";
+            }
+            ?>
+        </div>
+
+    </div> <!-- #main -->
+
+
+
+
+<?php include_once(INCLUDES_FOLDER_PATH . '/footer.php'); ?>
+
+
+
+
+<?php 
+
+// echo "<hr>";
+
+// echo "User Object<br/>";
+//     echo "<pre>";
+//     var_dump($user);
+//     echo "</pre>";
+
+// echo "<pre>";
+// var_dump($sessionRecord);
+// echo "</pre>";
+
 echo "<pre>";
-var_dump($session->exercises_performed);
+var_dump($session);
 echo "</pre>";
 
+// echo "<pre>";
+// var_dump($exerciseRecords);
+// echo "</pre>";
 
 ?>
