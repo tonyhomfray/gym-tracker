a8195c320b9831bc6da5bd10a6e5d18c10ed9b90
Tony Homfray <a.homfray@exeter.ac.uk>
2021-07-11 15:12:34 +0100

    Add exercise template html and css from codepen

---
+++ private/includes/exercise-template.php
@@ -0,0 +1,65 @@
+
+
+
+<div id="workout">Workout Input</div>
+<div id="exercise1" class="exercise">
+    <div class="exercise-row">
+    <span class="exercise-selector-header">Squat</span>
+    <span class="exercise-reps-header">Reps</span>
+    <span class="exercise-weight-header">Weight (kg)</span>
+    <span class="exercise-last-session-header">Last Session</span>
+    </div>
+    <div class="exercise-row">
+    <span class="row-header">Sets & Reps</span>
+    <span class="row-content-reps">
+        <div>
+        -&nbsp;&nbsp;8&nbsp;&nbsp;+
+        </div>
+        <div>
+        -&nbsp;&nbsp;8&nbsp;&nbsp;+
+        </div>
+        <div>
+        -&nbsp;&nbsp;8&nbsp;&nbsp;+
+        </div>
+    </span>
+    <span class="row-content-weight">
+        <div>
+        -&nbsp;&nbsp;25.25&nbsp;&nbsp;+
+        </div>
+        <div>
+        -&nbsp;&nbsp;25.25&nbsp;&nbsp;+
+        </div>
+        <div>
+        -&nbsp;&nbsp;25.25&nbsp;&nbsp;+
+        </div>
+    </span>
+    
+    <span class="row-content-last-session">
+        <span class="row-content-reps">
+        <div>8</div>
+        <div>8</div>
+        <div>6</div>
+        </span>
+        <span class="row-content-weight">
+        <div>25.00</div>
+        <div>25.00</div>
+        <div>25.00</div>
+    </span>
+    </div>
+    <div class="exercise-row">
+    <span class="row-header">Volume</span>
+    <span class="row-content-volume">606 kg</span>
+    <span class="row-content-volume-last-session">500 kg</span>
+    </div>
+    <div class="exercise-row">
+    <span class="row-header">Notes</span>
+    <span class="row-content-notes">
+        <span>These are my notes from this session.<br/>They don't tell you very much yet.</span>
+    </span>
+    <span class="row-content-notes-last-session">
+        <span>These are my notes from the last session.<br/>They don't tell you very much either.</span>
+    </span>  
+    </div>
+</div>
+<div id="exercise2" class="exercise">2</div>
+<div id="exercise3" class="exercise">3</div>  
