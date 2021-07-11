5cc8f6a9ae0827d306189180ab7c08df09d59389
Tony Homfray <a.homfray@exeter.ac.uk>
2021-07-11 16:11:30 +0100

    Move html head and nav into header.php

---
+++ private/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheets/main.css">
+    <title>Exercise Tracker - <?php echo $pageName; ?></title>
+</head>
+<body>
+    <nav id="top-nav">
+        <div id="nav-top-left">
+            <span>Nav1</span>
+            <span>Nav2</span>
+            <span>Nav3</span>
+        </div>
+        <div id="nav-top-right">
+            <span>Welcome back <?php echo $username; ?></span>
+        </div>
+    </nav>
